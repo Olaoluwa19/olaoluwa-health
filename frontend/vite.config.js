32ed85d19586d8d2281322190033267b9bb1a44d
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-07 12:37:39 +0100

    fix: did cleanup in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   resolve: {
+    alias: {
+      // This maps 'assets' to the actual folder path
+      'assets': path.resolve(__dirname, './src/assets'),
+    },
 });
