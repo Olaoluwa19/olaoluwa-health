7170acc212e34fd20f6fbb7be4258155c5edebd7
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-07 12:36:04 +0100

    fix: made adjustment to the router imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
 import Login from "./pages/Login";
-import About from "pages/About";
-import Contact from "pages/Contact";
-import MyProfile from "pages/MyProfile";
-import MyAppointments from "pages/MyAppointments";
-import Appointment from "pages/Appointment";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
