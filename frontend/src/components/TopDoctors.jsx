2d4f9fcc5cdb2803629b3a56c819ce5afad8e023
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-08 03:14:39 +0100

    feat: added use navigate to doctors card

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,6 +1,8 @@
 import { doctors } from "@/assets/assets_frontend/assets";
+import { useNavigate } from "react-router-dom";
 
 const TopDoctors = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
@@ -10,6 +12,7 @@ const TopDoctors = () => {
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
         {doctors.slice(0, 10).map((item, index) => (
           <div
+            onClick={() => navigate(`/appointment/${item._id}`)}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2.5 transition-all duration-500"
             key={index}
           >
