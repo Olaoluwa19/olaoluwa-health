07376313a5e128902062120d5e2d98e458049e24
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-08 11:48:43 +0100

    feat: completed homepage section

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* ------- Left Section -------- */}
         <div>
-          <img src={assets.logo_olaoluwa} alt="" />
-          <p>
+          <img className="mb-5 w-40" src={assets.logo_olaoluwa} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ea
             asperiores, dignissimos qui est mollitia fugit delectus aperiam,
             perferendis et odit? Minus, pariatur culpa. Doloribus error fuga
@@ -17,8 +17,8 @@ const Footer = () => {
 
         {/* ------- Center Section -------- */}
         <div>
-          <p>COMPANY</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>Home</li>
             <li>About us</li>
             <li>Contact us</li>
@@ -28,8 +28,8 @@ const Footer = () => {
 
         {/* ------- Right Section -------- */}
         <div>
-          <p>CONTACT DEVELOPER</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">CONTACT DEVELOPER</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>+234-906-775-9137</li>
             <li>oluwadurotimioladapo@gmail.com</li>
           </ul>
@@ -39,7 +39,9 @@ const Footer = () => {
       {/* --------- Copyright Text ---------- */}
       <div>
         <hr />
-        <p>Copyright ©️2026 Olaoluwa_codes - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">
+          Copyright ©2026 Oluwadurotimi Oladapo - All Right Reserved.
+        </p>
       </div>
     </div>
   );
