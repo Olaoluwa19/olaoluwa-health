aa366bec6b301bfab4f2834af8f5c991fba75f49
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-08 11:12:36 +0100

    feat: configured AppContext

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+import { doctors } from "@/assets/assets_frontend/assets";
+
+export const AppContext = createContext(null);
+
+/** @param {{ children: React.ReactNode }} props */
+const AppContextProvider = (props) => {
+  const value = {
+    doctors,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
