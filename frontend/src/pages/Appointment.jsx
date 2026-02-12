6ab3902764e2e974a34ee90729f08ebd91c7edc3
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-12 14:13:42 +0100

    feat: started styling the Appointments UI

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -12,7 +12,6 @@ const Appointment = () => {
   const fetchDocInfo = async () => {
     const docInfo = doctors.find((doc) => doc._id === docId);
     setDocInfo(docInfo);
-    console.log(docInfo);
   };
 
   useEffect(() => {
@@ -22,31 +21,43 @@ const Appointment = () => {
     docInfo && (
       <div>
         {/* ---------- Doctors Details ---------- */}
-        <div>
+        <div className="flex flex-col sm:flex-row gap-4">
           <figure>
-            <img src={docInfo.image} alt={docInfo.name} />
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt={docInfo.name}
+            />
           </figure>
-        </div>
 
-        <section>
-          {/* --------- Doc Info : name, degree, experience ---------- */}
-          <h2>
-            {docInfo.name} <img src={assets.verified_icon} alt="Verified" />
-          </h2>
-          <div>
+          <section className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 -mt-20 sm:mt-0">
+            {/* --------- Doc Info : name, degree, experience ---------- */}
+            <h2 className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}{" "}
+              <img className="w-5" src={assets.verified_icon} alt="Verified" />
+            </h2>
+            <figure className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
+            </figure>
+            {/* --------- Doctors About ---------- */}
+            <article>
+              <h3 className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="Info" />
+              </h3>
+              <p className="text-sm text-gray-500 max-w-175 mt-1">
+                {docInfo.about}
+              </p>
+            </article>
             <p>
-              {docInfo.degree} - {docInfo.speciality}
+              Appointment fees: <span>{docInfo.fees}</span>
             </p>
-            <button>{docInfo.experience}</button>
-          </div>
-          {/* --------- Doctors About ---------- */}
-          <article>
-            <h3>
-              About <img src={assets.info_icon} alt="Info" />
-            </h3>
-            <p>{docInfo.about}</p>
-          </article>
-        </section>
+          </section>
+        </div>
       </div>
     )
   );
