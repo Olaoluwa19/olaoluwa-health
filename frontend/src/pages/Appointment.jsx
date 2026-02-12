b435bf7308d65f8bf3d3a5be9a21716ba2671259
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-12 13:43:11 +0100

    feat: created the Doctors About UI and populated the docInfo's

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,3 +1,4 @@
+import { assets } from "@/assets/assets_frontend/assets";
 import { AppContext } from "@/context/AppContext";
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -11,12 +12,44 @@ const Appointment = () => {
   const fetchDocInfo = async () => {
     const docInfo = doctors.find((doc) => doc._id === docId);
     setDocInfo(docInfo);
+    console.log(docInfo);
   };
 
   useEffect(() => {
     fetchDocInfo();
   }, [doctors, docId]);
-  return <div>Appointment</div>;
+  return (
+    docInfo && (
+      <div>
+        {/* ---------- Doctors Details ---------- */}
+        <div>
+          <figure>
+            <img src={docInfo.image} alt={docInfo.name} />
+          </figure>
+        </div>
+
+        <section>
+          {/* --------- Doc Info : name, degree, experience ---------- */}
+          <h2>
+            {docInfo.name} <img src={assets.verified_icon} alt="Verified" />
+          </h2>
+          <div>
+            <p>
+              {docInfo.degree} - {docInfo.speciality}
+            </p>
+            <button>{docInfo.experience}</button>
+          </div>
+          {/* --------- Doctors About ---------- */}
+          <article>
+            <h3>
+              About <img src={assets.info_icon} alt="Info" />
+            </h3>
+            <p>{docInfo.about}</p>
+          </article>
+        </section>
+      </div>
+    )
+  );
 };
 
 export default Appointment;
