c5a0ed3035eb593ba7a8f3825d4160dbfcc21554
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-12 13:19:04 +0100

    feat: started working on the appointment page. Set up the necessary state management to fetch the doctors info

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { AppContext } from "@/context/AppContext";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Appointment = () => {
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
+
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+  };
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
   return <div>Appointment</div>;
 };
 
