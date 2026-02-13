115af1fdc2d1fd56008671e9ad7d162e6f355712
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-13 10:09:24 +0100

    feat: started working on related doctors component

---
+++ frontend/src/components/RelatedDoctors.jsx
@@ -0,0 +1,20 @@
+import { AppContext } from "@/context/AppContext";
+import { useContext, useEffect, useState } from "react";
+
+const RelatedDoctors = ({ speciality, docId }) => {
+  const { doctors } = useContext(AppContext);
+  const [relDoc, setRelDocs] = useState([]);
+
+  useEffect(() => {
+    if (doctors.length > 0 && speciality) {
+      const doctorsData = doctors.filter(
+        (doc) => doc.speciality === speciality && doc._id !== docId,
+      );
+      setRelDocs(doctorsData);
+    }
+  }, [doctors, speciality, docId]);
+
+  return <div></div>;
+};
+
+export default RelatedDoctors;
