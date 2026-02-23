b7c493e77188e7551cb791677460e2be42175968
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-23 03:37:21 +0100

    feat: connect to cloudinary

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import http from "http";
 import app from "./app.js";
 import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,6 +13,8 @@ const startServer = async () => {
   try {
     // connect to mongoDB
     connectDB();
+    // connect to cloudinary
+    connectCloudinary();
     mongoose.connection.once("open", () => {
       console.log("Connected to MongoDB");
       server.listen(PORT, () => {
