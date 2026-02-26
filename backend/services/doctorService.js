b1bdf684de2d2761e55176404791229ec5733239
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-02-26 08:00:54 +0100

    feat: created createDoctor method

--- backend/services/doctorService.js
+++ backend/services/doctorService.js
@@ -1,5 +1,33 @@
 import Doctor from "../models/Doctor.js";
 
-class DoctorService {}
+class DoctorService {
+  constructor() {}
+  static async createDoctor(doctorData) {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = doctorData;
+    const doctor = new Doctor({
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    });
+    await doctor.save();
+    return doctor;
+  }
+}
 
 export default new DoctorService();
