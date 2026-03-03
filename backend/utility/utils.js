c6128d0f8dd7d3d7381bc692c1debb64ce468822
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-03 04:26:23 +0100

    feat: setup utils files

---
+++ backend/utility/utils.js
@@ -0,0 +1,28 @@
+const validatePassword = (password) => {
+  const regex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{6,}$/;
+  return regex.test(password);
+};
+
+const getImage = async (req) => {
+  const fileName = req.file.filename;
+  const basePath = `${req.protocol}://${req.get("host")}/public/upload/`;
+  return `${basePath}${fileName}`;
+};
+
+const getGalleryImages = async (req) => {
+  const files = req.files;
+  let images = [];
+
+  const basePath = `${req.protocol}://${req.get("host")}/public/upload/`;
+
+  if (files && files.length > 0) {
+    images = files.map((file) => {
+      return `${basePath}${file.filename}`;
+    });
+  }
+
+  return images;
+};
+
+export { validatePassword, getImage, getGalleryImages };
