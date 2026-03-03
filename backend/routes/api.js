e1e341b75052010eda1db04cad87f0291ae6338f
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-03 04:22:47 +0100

    feat: mounted the adminRouter to api

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,11 @@
 import express from "express";
 import root from "./root.js";
+import adminRouter from "./api/doctorRoute.js";
 
 const api = express.Router();
 
 // Define API routes here
 api.use("/", root);
+api.use("/admin", adminRouter);
 
 export default api;
