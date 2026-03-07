dbde88b605541537c59ef3b850165b88a082c1f1
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-07 02:50:27 +0100

    added role to doctorsModel

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -4,6 +4,9 @@ const doctorSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    roles: {
+      type: [{ type: Number, enum: [5684] }],
+    },
     password: { type: String, required: true },
     image: { type: String, required: true },
     speciality: { type: String, required: true },
