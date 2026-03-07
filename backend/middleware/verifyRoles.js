647eba681ae3ec2559f23f4beed14f87077590ee
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-07 02:50:58 +0100

    feat: configured verify role function

---
+++ backend/middleware/verifyRoles.js
@@ -0,0 +1,15 @@
+const { unauthorized } = require("../utility/response");
+
+const verifyRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req?.roles) return unauthorized(res);
+    const rolesArray = [...allowedRoles];
+    const result = req.roles
+      .map((role) => rolesArray.includes(role))
+      .find((val) => val === true);
+    if (!result) return unauthorized(res);
+    next();
+  };
+};
+
+export default verifyRoles;
