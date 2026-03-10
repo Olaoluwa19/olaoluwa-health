3643e62b49b83e50cd5896e4f168c561dfe8101a
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-10 07:53:29 +0100

    feat: added GenSalt, evaluatePassword, generateAccessToken, generateRefreshToken, setRefreshTokenCookie functions

--- backend/utility/password.js
+++ backend/utility/password.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const validatePassword = (password) => {
   const regex =
@@ -6,8 +7,73 @@ const validatePassword = (password) => {
   return regex.test(password);
 };
 
-const encryptPassword = async (password) => {
-  return await bcrypt.hash(password, 10);
+const GenSalt = async () => {
+  return await bcrypt.genSalt(10);
 };
 
-export { validatePassword, encryptPassword };
+const encryptPassword = async (password) => {
+  const salt = await GenSalt();
+  return await bcrypt.hash(password, salt);
+};
+
+const evaluatePassword = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
+
+const generateAccessToken = (user, role) => {
+  if (!user || !role)
+    throw new Error("User and role are required to generate access token.");
+
+  if (!user.email && !user.phone)
+    throw new Error(
+      "User must have either email or phone to generate access token.",
+    );
+
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        user: user.email || user.phone,
+        roles: role,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" },
+  );
+
+  return accessToken;
+};
+
+const generateRefreshToken = (user) => {
+  if (!user) throw new Error("User is required to generate refresh token.");
+
+  if (!user.email && !user.phone)
+    throw new Error(
+      "User must have either email or phone to generate refresh token.",
+    );
+
+  const refreshtoken = jwt.sign(
+    { email: user.email, phone: user.phone },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  return refreshtoken;
+};
+
+const setRefreshTokenCookie = (res, refreshToken) => {
+  return res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+};
+
+export {
+  validatePassword,
+  encryptPassword,
+  evaluatePassword,
+  generateAccessToken,
+  generateRefreshToken,
+  setRefreshTokenCookie,
+};
