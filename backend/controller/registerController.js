b16814062d0c67993b66721835bf54c296f83023
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-12 06:39:15 +0100

    feat: created register controller function

--- backend/controller/registerController.js
+++ backend/controller/registerController.js
@@ -1,8 +1,12 @@
-import { badRequest } from "../utility/response";
+import { v2 as cloudinary } from "cloudinary";
+import UserService from "../services/userService.js";
+import DoctorService from "../services/doctorService.js";
+import { validatePassword, hashPassword } from "../utility/password.js";
+import { badRequest, conflict } from "../utility/response.js";
 
 const createNewUser = async (req, res) => {
   // validate user roles
-  const { username, password, roles, address } = req.body;
+  const { email, password, roles, doctorFields } = req.body;
 
   if (![5684, 1973, 3956].includes(roles)) {
     return badRequest(res, `Invalid role specified, ${roles} is not allowed.`);
@@ -23,34 +27,53 @@ const createNewUser = async (req, res) => {
 
   // check for duplicate user
 
-  const duplicate = await checkDuplicateUser(username, roles);
+  const duplicate = await UserService.checkDuplicateUser(email, roles);
 
   if (duplicate) {
-    return responseMessage(
-      res,
-      409,
-      false,
-      "User already exists. Please login.",
-    );
-    // conflict
+    return conflict(res, "User already exists. Please login."); // conflict
   }
 
-  try {
-    // create address document
-    const addressDoc = await createAddress({ body: { ...address } });
+  // handles image upload
+  const file = req.file;
+  if (!file) {
+    return badRequest(res, "Image file is required");
+  }
 
-    // encrypt the password
-    const hashedPwd = await encryptPassword(password);
+  // Image upload to cloudinary
+  const imageUpload = await cloudinary.uploader.upload(file.path, {
+    resource_type: "image",
+    folder: "doctors",
+  });
+
+  const imageUrl = imageUpload.secure_url;
+
+  try {
+    // create doctor document
+    if (roles === 1973) {
+      if (!doctorFields) {
+        return badRequest(res, "Doctor fields are required for doctor role");
+      }
+    }
+    const doctorDoc = await DoctorService.createDoctorField({
+      body: { ...doctorFields },
+    });
+
+    // hash the password
+    const hashedPwd = await hashPassword(password);
 
     // createUserFields document
-    const newUser = await createUserFields(req, hashedPwd, addressDoc._id);
+    const newUser = await UserService.createUserField(
+      req,
+      hashedPwd,
+      imageUrl,
+      doctorDoc._id,
+    );
 
-    const populatedUser = await getPopulatedUser(newUser._id);
+    const populatedUser = await UserService.populateUser(newUser._id);
 
-    console.log(populatedUser);
-    return res.status(201).json(populatedUser);
+    return created(res, populatedUser, "User Registered successfully☑️!");
   } catch (error) {
-    return serverError(res, error);
+    return serverError(res, "Failed to Register new User", error);
   }
 };
 
