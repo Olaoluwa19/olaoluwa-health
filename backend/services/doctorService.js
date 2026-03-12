1c9fbe32a06cb770194972623a97ba4d6b3d8f1e
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-12 06:41:16 +0100

    modified doctorService class and removed redundant methods

--- backend/services/doctorService.js
+++ backend/services/doctorService.js
@@ -1,39 +1,17 @@
 import Doctor from "../models/doctorModel.js";
-import validator from "validator";
 
 class DoctorService {
   constructor() {}
 
-  static async createDoctorField(request, hashPassword, role, imageUrl) {
-    const {
-      name,
-      email,
-      speciality,
-      degree,
-      experience,
-      about,
-      fees,
-      address,
-    } = request.body;
-
-    // Basic validations
-    if (!imageUrl) throw new Error("Image upload failed");
-    if (!name || !email) throw new Error("Name & email required");
-    // Simple email format validation
-    if (!validator.isEmail(email)) throw new Error("Invalid email format");
+  static async createDoctorField(request) {
+    const { speciality, degree, experience, about, available } = request.body;
 
     const doctor = await Doctor.create({
-      name: name.trim(),
-      email: email.toLowerCase().trim(),
-      password: hashPassword,
       speciality,
       degree,
       experience,
       about,
-      fees: Number(fees),
-      address: JSON.parse(address),
-      roles: [role],
-      image: imageUrl,
+      available,
     });
 
     return doctor;
@@ -43,21 +21,7 @@ class DoctorService {
     return await Doctor.findOne({ _id: userId }).exec();
   }
 
-  static async checkDuplicateDoctor(username, roles) {
-    return await Doctor.findOne({ username: username, roles: roles }).exec();
-  }
-
-  static async findDoctorByEmailOrPhone(identifier) {
-    return await Doctor.findOne({
-      $or: [{ phone: identifier }, { email: identifier }],
-    }).exec();
-  }
-
-  static async encryptPassword(password) {
-    return await bcrypt.hash(password, 10);
-  }
-
-  static async deleteDeleteFields(id) {
+  static async deleteDoctorFields(id) {
     return await Doctor.deleteOne(id);
   }
 
