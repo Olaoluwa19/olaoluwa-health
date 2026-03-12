d6b0dc6530b6ae638e903e6536a2162e6c08ff7a
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-12 06:39:49 +0100

    feat: made error handling more robust

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -3,11 +3,12 @@ import { forbidden, unauthorized } from "../utility/response";
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!authHeader?.startsWith("Bearer ")) return unauthorized(res); // Bearer Token missing
+  if (!authHeader?.startsWith("Bearer "))
+    return unauthorized(res, "No Bearer Token found🔐!"); // Bearer Token missing
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return forbidden(res); // invalid token
-    req.user = decoded.UserInfo.email;
+    if (err) return forbidden(res, "Invalid Token provided🔐!"); // invalid token
+    req.user = decoded.UserInfo.email || decoded.UserInfo.phone;
     req.roles = decoded.UserInfo.roles;
     next();
   });
