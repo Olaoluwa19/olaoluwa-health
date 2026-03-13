5b3841068c649efb3763f541c9e06b4646f923c1
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-13 04:21:52 +0100

    feat: mount register and login router

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,11 +1,15 @@
 import express from "express";
 import root from "./root.js";
 import adminRouter from "./api/doctorRoute.js";
+import registerRouter from "./register.js";
+import LoginRouter from "./login.js";
 
 const api = express.Router();
 
 // Define API routes here
 api.use("/", root);
+api.use("/register", registerRouter);
+api.use("/login", LoginRouter);
 api.use("/admin", adminRouter);
 
 export default api;
