c0feaf355a50e573157ca6c11364988674e10853
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-14 19:17:59 +0100

    feat: modified and made handleLogin more roburst to accomodate all users login logic

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,19 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import Doctor from "../models/doctorModel";
+import User from "../models/userModel.js";
+import UserService from "../services/userService.js";
 import {
   badRequest,
   forbidden,
-  serverError,
   unauthorized,
-} from "../utility/response";
-import { evaluatePassword, generateAccessToken } from "../utility/password";
+  created,
+} from "../utility/response.js";
+import {
+  comparePassword,
+  generateAccessToken,
+  generateRefreshToken,
+  setRefreshTokenCookie,
+} from "../utility/password.js";
 
-const handleDoctorLogin = async (req, res) => {
+const handleUserLogin = async (req, res) => {
   const { phone, email, password } = req.body;
 
   // require email or phone for login
@@ -23,40 +27,44 @@ const handleDoctorLogin = async (req, res) => {
   if (!password) return badRequest(res, "Password is required to login.");
 
   //   Check if user exists
-  const foundDoctor = await Doctor.findDoctorByEmailOrPhone(loginIdentifier);
+  const foundUser = await UserService.findUserByEmailOrPhone(loginIdentifier);
 
-  if (!foundDoctor)
-    return unauthorized(res, "The Doctor details provided does not exist"); // unauthorised
+  if (!foundUser)
+    return unauthorized(res, "The User details provided does not exist"); // unauthorised
 
-  if (foundDoctor.isSuspended)
-    return forbidden(res, "Doctor is suspended from accessing the platform."); // forbidden
+  if (foundUser.isSuspended)
+    return forbidden(res, "User is suspended from accessing the platform😡!"); // forbidden
 
   // Evaluate password
-  const match = evaluatePassword(password, foundDoctor.password);
+  const match = comparePassword(password, foundUser.password);
 
   if (match) {
-    const role = Object.values(foundDoctor.roles);
+    const role = Object.values(foundUser.roles);
 
     //create JWT's
-    const accessToken = generateAccessToken(foundDoctor, role);
-    const refreshToken = generateRefreshToken(foundDoctor);
+    const accessToken = generateAccessToken(foundUser, role);
+    const refreshToken = generateRefreshToken(foundUser);
 
     // Saving refreshToken with current user
-    foundDoctor.refreshToken = refreshToken;
-    const doctor = await foundDoctor.save();
-    const populatedDoctor = await Doctor.populatedDoctor(doctor._id);
-    console.log(populatedDoctor);
+    foundUser.refreshToken = refreshToken;
+    const user = await foundUser.save();
+    const populatedUser = await UserService.populateUser(user._id);
+    console.log(populatedUser);
 
     // set cookie with refresh token
     setRefreshTokenCookie(res, refreshToken);
 
-    return created(res, "Login successful", {
-      doctor: populatedDoctor,
-      accessToken,
-    });
+    return created(
+      res,
+      {
+        accessToken,
+        user: populatedUser,
+      },
+      "Login successful☺️🔓",
+    );
   } else {
-    return unauthorized(res, "Login failed. Invalid credentials.");
+    return unauthorized(res, "Login failed😔! Invalid credentials🗝️.");
   }
 };
 
-module.exports = { handleDoctorLogin };
+export default handleUserLogin;
