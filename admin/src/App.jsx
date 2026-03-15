5913dc2078523ecf3a26097382ab82d9afcd04cb
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-15 06:15:50 +0100

    fix: renamed div to body for semantics and accessibility

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-green-500">App</div>;
+  return (
+    <body>
+      <Login />
+    </body>
+  );
 };
 
 export default App;
